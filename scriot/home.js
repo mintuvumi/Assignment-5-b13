07c9dc34fa6f6c73d00ba25b77a51232d375af1d
MinTuVumi <mdmozammlhossain10@gmail.com>
2026-03-09 20:54:57 +0600

    Home Page is captured with js

---
+++ scriot/home.js
@@ -0,0 +1,10 @@
+const container=document.getElementById("issuesContainer")
+const loader=document.getElementById("loader")
+
+const allBtn=document.getElementById("allBtn")
+const openBtn=document.getElementById("openBtn")
+const closedBtn=document.getElementById("closedBtn")
+
+const search=document.getElementById("search")
+
+let issues=[]
